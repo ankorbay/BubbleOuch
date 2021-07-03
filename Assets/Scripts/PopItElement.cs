86c52be2918e6ea5af5072f3a565a919ee6e691d
Anton Korshunov <anton.korshunov@banuba.com>
2021-07-04 01:27:52 +0300

    prototype work without reset

---
+++ Assets/Scripts/PopItElement.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+public enum eBaloonState
+{
+    FULL,
+    BLOWS_OFF,
+    EMPTY
+}
+
+public class PopItElement : MonoBehaviour
+{
+    [SerializeField] [Range(-40f, 20f)] float zValue = -40f;
+
+    Vector3 touchPosWorld;
+    bool areNormalsFlipped = false;
+
+    TouchPhase touchPhase = TouchPhase.Ended;
+
+    void Start()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, zValue, transform.localScale.z);
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase)
+        {
+            touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+            Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
+
+            RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+
+            if (hitInformation.collider != null)
+            {
+                GameObject touchedObject = hitInformation.transform.gameObject;
+                Debug.Log("Touched " + touchedObject.transform.name);
+                touchedObject.transform.DOScaleY(20f, 1f);
+            }
+        }
+
+        if (transform.localScale.y >= 0f && !areNormalsFlipped) FlipNormals();
+    }
+
+    void FlipNormals()
+    {
+        print("flip");
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+
+        Vector3[] normals = mesh.normals;
+        for (int i = 0; i < normals.Length; i++)
+        {
+            normals[i] = -1 * normals[i];
+        }
+        mesh.normals = normals;
+
+        for (int i = 0; i < mesh.subMeshCount; i++)
+        {
+            int[] tris = mesh.GetTriangles(i);
+            for(int j = 0; j < tris.Length; j += 3)
+            {
+                int temp = tris[j];
+                tris[j] = tris[j + 1];
+                tris[j + 1] = temp;
+            }
+            mesh.SetTriangles(tris, i);
+        }
+
+        areNormalsFlipped = true;
+    }
+
+    public void Push()
+    {
+        transform.DOScaleY(20f, 1f);
+    }
+}
---
+++ Assets/Scripts/SelectOnClick.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class SelectOnClick : MonoBehaviour
+{
+    [SerializeField] GameObject plane;
+
+    public GameObject selectedObject;
+    public GameObject highlightedObject;
+    public LayerMask selectableLayer;
+    Ray ray;
+    RaycastHit hitData;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitData, 1000, selectableLayer))
+        {
+            highlightedObject = hitData.transform.gameObject;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                selectedObject = hitData.transform.gameObject;
+
+                PopItElement shpere = selectedObject.GetComponent<PopItElement>();
+
+                if (shpere != null)
+                {
+                    shpere.Push();
+                    MovePlaneAfterShpereTouch(selectedObject);
+                }
+            }
+        }
+        else
+        {
+            highlightedObject = null;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                selectedObject = null;
+            }
+        }
+    }
+
+    void MovePlaneAfterShpereTouch(GameObject selectedObject)
+    {
+        if(selectedObject.transform.localPosition.x == 0 && selectedObject.transform.localPosition.z == 0)
+        {
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(plane.transform.DOMoveZ(plane.transform.position.z + 0.1f, 0.2f));
+            sequence.Append(plane.transform.DOMoveZ(plane.transform.position.z, 0.2f));
+        }
+        else
+        {
+            float currentXrotation = plane.transform.eulerAngles.x;
+            float currentYrotation = plane.transform.eulerAngles.y;
+            float currentZrotation = plane.transform.eulerAngles.z;
+
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(plane.transform.DORotate(
+                new Vector3(currentXrotation - selectedObject.transform.position.z,
+                            currentYrotation,
+                            currentZrotation - selectedObject.transform.position.x),
+                0.2f));
+            sequence.Append(plane.transform.DORotate(
+                new Vector3(currentXrotation,
+                            currentYrotation,
+                            currentZrotation),
+                0.2f));
+        }
+    }
+}
