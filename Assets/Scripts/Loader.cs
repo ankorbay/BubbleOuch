e41b1ccbd3d0e0c9d07dcacb6802fbf5b9e062eb
Anton Korshunov <anton.korshunov@banuba.com>
2021-07-08 19:15:55 +0300

    Loader fixed to change models in runtime

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,37 +1,62 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Loader : MonoBehaviour
 {
     [SerializeField] Transform rootObjectTransform;
-    
     [SerializeField] PopitMeshes popitMeshes;
 
+    private Vector3 defaultMeshRotation = new Vector3(-90, 0f, 0f); // TODO update after models fix
     Dictionary<int,GameObject> InstantiatedGameObjects;
     GameObject currentlyActiveGO;
+    
+    
     void Start()
     {
         InstantiatedGameObjects = new Dictionary<int, GameObject>();
-        Load(1);
+        Load(0);
+
+        // TestRuntimeSpawn();
+    }
+
+    
+    void TestRuntimeSpawn()
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.PrependInterval(4f);
+        sequence.AppendCallback(() => { Load(1); });
+        sequence.AppendInterval(4f);
+        sequence.OnComplete(() => { Load(0); });
     }
 
     void Load(int index)
     {
         if (InstantiatedGameObjects.Count > 0)
         {
+            currentlyActiveGO.gameObject.SetActive(false);
+            
+            print("InstantiatedGameObjects.Count="+InstantiatedGameObjects.Count);
             if (InstantiatedGameObjects.ContainsKey(index))
             {
-                currentlyActiveGO.SetActive(false);
                 InstantiatedGameObjects[index].gameObject.SetActive(true);
+            }
+            else
+            {
+                SpawnEntity(index);
             }
         }
         else
         {
-            currentlyActiveGO = Instantiate(popitMeshes.prefabs[index], Vector3.zero, Quaternion.identity);
-            currentlyActiveGO.transform.parent = rootObjectTransform;
-            InstantiatedGameObjects.Add(index,currentlyActiveGO);
+            SpawnEntity(index);
         }
-        
+    }
+
+    void SpawnEntity(int index)
+    {
+        currentlyActiveGO = Instantiate(popitMeshes.prefabs[index], Vector3.zero, Quaternion.Euler(defaultMeshRotation));
+        currentlyActiveGO.transform.parent = rootObjectTransform;
+        InstantiatedGameObjects.Add(index,currentlyActiveGO);
     }
 }
